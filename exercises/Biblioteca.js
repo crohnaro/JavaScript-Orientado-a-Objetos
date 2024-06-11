b3d00da265c7b3371541b14e83466cfbad1c111d
taissilva2604 <taisinha2604@gmail.com>
2024-06-11 16:25:51 -0300

    Solução dos exercícios

--- exercises/Biblioteca.js
+++ exercises/Biblioteca.js
@@ -2,30 +2,58 @@
 
 class Livro {
   constructor(titulo, autor) {
-    
+    this.titulo = titulo;
+    this.autor = autor;
+    this.disponivel = true; 
   }
 
   emprestar() {
-    
+    if (this.disponivel) {
+      this.disponivel = false;
+    return true;
+  } else {
+
+    return false;
   }
+}
 
   devolver() {
+    if (!this.disponivel) {
+      this.disponivel = true;
+      return `O livro "${this.titulo}" foi devolvido`;
+
+    }
+    else `O livro "${this.titulo}" não foi emprestado`;
     
   }
 }
 
 class Biblioteca {
   constructor() {
+    this.livros = [];
     
   }
 
   adicionarLivro(livro) {
+    this.livros.push(livro);
+    return `O livro "${livro.titulo}" foi adicionado à biblioteca`;
    
   }
 
   listarLivros() {
-  
+    if (this.livros.length === 0) {
+      return "A biblioteca está vazia.";
+    } else {
+      return this.livros.map(livro => {
+        return {
+          titulo: livro.titulo,
+          autor: livro.autor,
+          disponivel: livro.disponivel
+        };
+      });
+    }
   }
 }
+
 
 module.exports = { Livro, Biblioteca };
