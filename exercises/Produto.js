7b6400222b2496e7dbe66b755063d360117242ea
Welker42 <welkerandrade@gmail.com>
2024-06-11 15:48:00 -0300

    Solução dos exercícios

--- exercises/Produto.js
+++ exercises/Produto.js
@@ -1,16 +1,24 @@
-//Crie uma classe Produto com propriedades nome, preco e quantidade. Adicione métodos para adicionarEstoque() e vender().
-
 class Produto {
   constructor(nome, preco, quantidade) {
-    
+    this.nome = nome;
+    this.preco = preco;
+    this.quantidade = quantidade;
   }
 
   adicionarEstoque(quantidade) {
-    
+    if (quantidade > 0) {
+      this.quantidade += quantidade;
+      return true;
+    }
+    return false;
   }
 
   vender(quantidade) {
-    
+    if (quantidade > 0 && quantidade <= this.quantidade) {
+      this.quantidade -= quantidade;
+      return true;
+    }
+    return false;
   }
 }
 
