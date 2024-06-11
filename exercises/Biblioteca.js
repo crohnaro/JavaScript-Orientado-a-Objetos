0090446f143c1effc5bd4e4b6d0e24a16c606a9f
Vale-JP <dmoreiravale@gmail.com>
2024-06-11 16:06:06 -0300

    Soluções dos exercícios

--- exercises/Biblioteca.js
+++ exercises/Biblioteca.js
@@ -2,30 +2,59 @@
 
 class Livro {
   constructor(titulo, autor) {
-    
+      this.titulo = titulo;
+      this.autor = autor;
+      this.disponivel = true;
   }
 
   emprestar() {
-    
+      if (this.disponivel) {
+          this.disponivel = false;
+          return true;
+      } else {
+          return false;
+      }
   }
 
   devolver() {
-    
+      this.disponivel = true;
   }
 }
 
 class Biblioteca {
   constructor() {
-    
+      this.livros = [];
   }
 
   adicionarLivro(livro) {
-   
+      this.livros.push(livro);
   }
 
   listarLivros() {
-  
+      return this.livros.map(livro => ({
+          titulo: livro.titulo,
+          autor: livro.autor,
+          disponivel: livro.disponivel
+      }));
+  }
+
+  emprestarLivro(tituloLivro) {
+      const livro = this.livros.find(livro => livro.titulo === tituloLivro);
+      if (livro) {
+          return livro.emprestar();
+      } else {
+          return false;
+      }
+  }
+
+  devolverLivro(tituloLivro) {
+      const livro = this.livros.find(livro => livro.titulo === tituloLivro);
+      if (livro) {
+          livro.devolver();
+          return true;
+      } else {
+          return false;
+      }
   }
 }
-
 module.exports = { Livro, Biblioteca };
