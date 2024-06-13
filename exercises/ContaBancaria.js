2292925389000a2297e70ab8bb58021d0d340569
Haz-aro <yannoronhabr@gmail.com>
2024-06-13 14:22:21 -0300

    Solução dos exercícios

--- exercises/ContaBancaria.js
+++ exercises/ContaBancaria.js
@@ -1,20 +1,33 @@
-//Crie uma classe ContaBancaria com propriedades numeroDaConta, saldo e métodos depositar() e sacar(). Adicione validações para garantir que não seja possível sacar mais do que o saldo disponível.
 class ContaBancaria {
   constructor(numeroDaConta, saldo = 0) {
-    
+    this.numeroDaConta = numeroDaConta
+    this.saldo = saldo
   }
 
   depositar(valor) {
-    
+    if (valor > 0) {
+      this.saldo += valor
+      return true
+    } 
+    else {
+      return false
+    }
   }
 
   sacar(valor) {
-    
+    if (valor > 0 && valor <= this.saldo) {
+      this.saldo -= valor
+      return true
+    } 
+    else {
+      return false
+    }
   }
 
   verificarSaldo() {
-    
+    return this.saldo
   }
 }
 
-module.exports = ContaBancaria;
+module.exports = ContaBancaria
+
--- exercises/Produto.js
+++ exercises/Produto.js
@@ -1,17 +1,25 @@
-//Crie uma classe Produto com propriedades nome, preco e quantidade. Adicione métodos para adicionarEstoque() e vender().
-
 class Produto {
   constructor(nome, preco, quantidade) {
-    
+    this.nome = nome;
+    this.preco = preco;
+    this.quantidade = quantidade;
   }
 
   adicionarEstoque(quantidade) {
-    
+    if (quantidade > 0) {
+      this.quantidade += quantidade;
+    }
   }
 
   vender(quantidade) {
-    
+    if (quantidade > 0 && quantidade <= this.quantidade) {
+      this.quantidade -= quantidade;
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
 module.exports = Produto;
+
