2fad603a5d592652892c3ed7f1a88d2016103f6a
rafaelmarcx <kiba.games123@gmail.com>
2024-06-13 14:51:53 -0300

    Solução dos exercícios

--- exercises/ContaBancaria.js
+++ exercises/ContaBancaria.js
@@ -1,20 +1,27 @@
 //Crie uma classe ContaBancaria com propriedades numeroDaConta, saldo e métodos depositar() e sacar(). Adicione validações para garantir que não seja possível sacar mais do que o saldo disponível.
 class ContaBancaria {
-  constructor(numeroDaConta, saldo = 0) {
-    
+  constructor(numero, saldoInicial = 0) {
+      this.numero = numero;
+      this.saldo = saldoInicial;
   }
 
   depositar(valor) {
-    
+      this.saldo += valor;
   }
 
   sacar(valor) {
-    
+      if (valor <= this.saldo) {
+          this.saldo -= valor;
+          return true;
+      } else {
+          return false;
+      }
   }
 
   verificarSaldo() {
-    
+      return this.saldo;
   }
 }
 
 module.exports = ContaBancaria;
+
--- exercises/Estacionamento.js
+++ exercises/Estacionamento.js
@@ -2,26 +2,37 @@
 
 class Carro {
   constructor(placa, modelo) {
-    
+      this.placa = placa;
+      this.modelo = modelo;
   }
 }
 
 class Estacionamento {
   constructor() {
-    
+      this.carrosEstacionados = [];
   }
 
   estacionarCarro(carro) {
-    
+      this.carrosEstacionados.push(carro);
   }
 
   removerCarro(placa) {
-    
+      const index = this.carrosEstacionados.findIndex(carro => carro.placa === placa);
+      if (index !== -1) {
+          this.carrosEstacionados.splice(index, 1);
+          return true;
+      } else {
+          return false;
+      }
   }
 
   listarCarros() {
-    
+      return this.carrosEstacionados.map(carro => ({
+          placa: carro.placa,
+          modelo: carro.modelo
+      }));
   }
 }
 
 module.exports = { Carro, Estacionamento };
+
